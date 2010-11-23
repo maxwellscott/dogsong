bf5fee5979711bf50cf98959ce38ea499bf3330e
Max <msprior@gmail.com>
2010-11-23 01:15:56 -0500

    dogsong sings

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dogsong::Application.config.secret_token = '9e9c17d978faf22facc0b0c5d139f9004c5d1ea9d94e36a4ae972c841e4617971322a4d7589de792dc33d98516311cb44586f0ac12c6e4aeb1051279198d5e82'
